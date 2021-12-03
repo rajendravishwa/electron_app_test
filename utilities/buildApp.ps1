efeeb80b99b2f868fde5da6c85c24e7373fd9853
rajendravishwa <raj.special12@hotmail.com>
2021-12-03 13:50:17 -0800

    rrr

--- utilities/buildApp.ps1
+++ utilities/buildApp.ps1
@@ -9,7 +9,7 @@ if (!(Test-Path -LiteralPath "$currentLocation\node_modules\electron-packager"))
     npm install --prefix .\wmi_panel\node_modules\electron-packager --save-dev electron-packager
 }
 Write-Output "running elec packager"
-npx electron-packager $currentLocation "PerfMonitor" --overwrite --platform=win32 --arch=x64 --icon=./assets/icon.ico
+npx electron-packager .\wmi_panel "PerfMonitor" --overwrite --platform=win32 --arch=x64 --icon=./assets/icon.ico
 $buildfolder = Get-ChildItem -Directory -Path $currentLocation -Filter "PerfMonitor*"
 Write-Output "Packaging build into $buildfolder.zip"
 Compress-Archive -Path $buildfolder -Force -DestinationPath "$buildfolder.zip"
