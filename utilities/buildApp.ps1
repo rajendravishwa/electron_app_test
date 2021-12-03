53a85e27bbaecb38301727f71ebff3cb417c2365
rajendravishwa <raj.special12@hotmail.com>
2021-12-03 13:46:14 -0800

    rr

--- utilities/buildApp.ps1
+++ utilities/buildApp.ps1
@@ -1,14 +1,14 @@
 $currentLocation = Get-Location
-
+Write-Output "installing modules"
 # Perform npm install if node_modules folder doesn't exist
 if (!(Test-Path -LiteralPath "$currentLocation\node_modules")) {
     npm install --prefix .\wmi_panel
 }
-
+Write-Output "install elec packager"
 if (!(Test-Path -LiteralPath "$currentLocation\node_modules\electron-packager")) {
     npm install --prefix .\wmi_panel\node_modules\electron-packager --save-dev electron-packager
 }
-
+Write-Output "running elec packager"
 npx electron-packager $currentLocation "PerfMonitor" --overwrite --platform=win32 --arch=x64 --icon=./assets/icon.ico
 $buildfolder = Get-ChildItem -Directory -Path $currentLocation -Filter "PerfMonitor*"
 Write-Output "Packaging build into $buildfolder.zip"
