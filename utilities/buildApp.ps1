36bb97faf855cfced05fd15563da4d2af8251faa
rajendravishwa <raj.special12@hotmail.com>
2021-12-02 17:48:19 -0800

    build  path update

--- utilities/buildApp.ps1
+++ utilities/buildApp.ps1
@@ -5,6 +5,6 @@ if (!(Test-Path -LiteralPath "$currentLocation\node_modules")) {
     npm install
 }
 
-npx electron-packager wmi_scanner_article-master "PerfMonitor" --overwrite
+npx electron-packager $currentLocation "PerfMonitor" --overwrite
 Compress-Archive -Path PerfMonitor-win32-x64 -DestinationPath PerfMonitor-win32-x64.zip
 Remove-Item PerfMonitor-win32-x64 -Recurse
